139a7a8339004387b4ec8c1867047095a761d891
Aditi <adi.vishwakarma01@gmail.com>
2023-10-09 17:57:54 +0530

    added a dummy url

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Welcome to travel Log");
+});
 app.use("/posts", postRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 
